1693628ae325a28a74ccfa9c05c1e4258a7c90ea
lai2-24 <lai2-24@itismeucci.com>
2024-10-22 14:31:47 +0200

    secondo commit

--- src/main/java/com/example/MioThread.java
+++ src/main/java/com/example/MioThread.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class MioThread extends Thread {
@@ -18,42 +19,38 @@ public class MioThread extends Thread {
     public void run() {
         try {
             int conta = 0;
+            int var=0;
             String stringRead = "";
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
             do {
+                conta ++;
                 stringRead = in.readLine();
-                System.out.println("Stringa ricevuta : " + stringRead);
 
-                
-                
-                if (Integer.parseInt(stringRead) < numeroDaIndovinare) {
-                    System.out.println("<");
-                    conta ++;
+                var = Integer.parseInt(stringRead);
+                if(var>numeroDaIndovinare && var<=100 ){
+                    out.writeBytes(">" + '\n');
                 }
 
-
-
-                if (Integer.parseInt(stringRead) > numeroDaIndovinare) {
-                    System.out.println(">");
-                    conta ++;
+                if(var<numeroDaIndovinare && var>0){
+                    out.writeBytes("<" + '\n');
                 }
-
-                if (Integer.parseInt(stringRead) == numeroDaIndovinare) {
-                    System.out.println("= in " + conta + "tentativi");
-                    System.out.println("Nuova partita? (yes, no)");
-                    if (stringRead.equals("no")) {
-                        System.out.println("The client wants to close");
-                        s.close();
-                    }
+                
+                if(var == numeroDaIndovinare){
+                    out.writeBytes("=" + '\n');
+                    out.writeBytes(Integer.toString(conta) + '\n');
+                    break;
                 }
-
+                
+                if(var <= 0 || var > 100 ){
+                    out.writeBytes("numero non compreso " + '\n');
+                }
                 
 
-                
-
-            out.writeBytes(stringRead + '\n');
             } while (true);
+
+            
+
         } catch (IOException e) {
             System.out.println("ERROR");
             System.exit(1);
